1d1e4e00e14a4010af8dc46f60dd183d621e9360
Stefan <stefan.panic@outlook.com>
2017-01-03 23:58:52 +0100

    added before filters

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,6 @@
 class ProfilesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :only_current_user
 
   def new
     @profile = Profile.new
@@ -36,5 +38,10 @@ end
 private
   def profile_params
     params.require(:profile).permit(:first_name, :last_name, :avatar, :contact_email, :description)
+  end
+
+  def only_current_user
+    @user = User.find( params[:user_id] )
+    redirect_to(root_url) unless @user == current_user
   end
 end
