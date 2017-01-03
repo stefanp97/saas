4b28e4b462bd2afe0277192959bf5b52fa5555b9
Stefan <stefan.panic@outlook.com>
2017-01-03 21:48:14 +0100

    added imagemagic and paperclip gem

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,8 @@
 class ProfilesController < ApplicationController
+
+  def new
+    @profile = Profile.new
+  end
   # POST to /users/:user_id/profile
 def create
   # Ensure that we have the user who is filling out form
@@ -6,8 +10,8 @@ def create
   # Create profile linked to this specific user
   @profile = @user.build_profile( profile_params )
   if @profile.save
-    flash[:success] = "Profile updated!"
-    redirect_to root_path
+    # flash[:success] = "Profile updated!"
+    redirect_to user_path( params[:user_id] )
   else
     render action: :new
   end
